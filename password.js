e6b3a4f799a4113727614757e625d9a4c3b6eee7
pantheonsh <pantheonshell@yahoo.com>
2018-10-30 15:08:50 -0300

    Password Manager skeleton, sqlite dep

---
+++ password.js
@@ -0,0 +1,20 @@
+const sqlite = require("better-sqlite3");
+
+class PasswordManager {
+    /**
+     * @param {String} master_key 
+     */
+    constructor(master_key) {
+        
+    }
+
+    getAllRecords() {
+
+    }
+
+    getPassword(record_id) {
+
+    }
+}
+
+module.exports = PasswordManager;
